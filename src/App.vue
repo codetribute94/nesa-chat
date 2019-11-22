a9af8ff69402d9586df218543c9d135fa178cf8a
Solomon okafor <Solomon@Solomon-okafors-MacBook.local>
2019-11-22 11:03:03 -0500

    created new files and dependencies

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: "Hello World",
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
