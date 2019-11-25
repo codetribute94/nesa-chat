d3ac0b666f7c08b6af8c12bd146116bc849e7425
IdheeNkanga <idongesitnkanga96@gmail.com>
2019-11-25 15:31:30 +0100

    completed signup page

--- src/views/signup.vue
+++ src/views/signup.vue
@@ -1,5 +1,15 @@
 <template>
-    
+<div>
+    <form class="box" action="" method="POST">
+        <h1>signup</h1>
+        <input class="types" type="text" name="" placeholder="fullname">
+        <input class="types" type="text" name="" placeholder="username">
+        <input class="types" type="email" name="" placeholder="email">
+        <input class="types" type="password" name="" placeholder="password">
+        <input class="types" type="password" name="" placeholder="confirm password">
+        <input type="submit" name="" value="signup">
+    </form>
+</div>
 </template>
 
 <script>
@@ -9,5 +19,54 @@
 </script>
 
 <style scoped>
+.box{
+    width: 320px;
+    padding: 40px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    background: #191919;
+    text-align: center;
+    transform: translate(-50%, -50%);
+}
 
+.box h1{
+    color: white;
+    text-transform: uppercase;
+    font-weight: 500;
+}
+
+.types{
+    border: 0;
+    background: none;
+    display: block;
+    margin: 20px auto;
+    text-align: center;
+    border: 2px solid #3498db;
+    padding: 14px 10px;
+    width: 240px;
+    outline: none;
+    color: white;
+    border-radius: 24px;
+}
+.types:focus{
+    width: 300px;
+    color: #2ecc71;
+}
+.box input[type ='submit']{
+    width: 120px;
+    background: none;
+    display: block;
+    margin: 20px, auto;
+    margin-left: 95px;
+    text-align: center;
+    align-content: center;
+    border: 2px solid #2ecc71;
+    padding: 14px 40px;
+    outline: none;
+    color: white;
+    border-radius: 24px;
+    cursor: pointer;
+    transition: 0.25s;
+}
 </style>
