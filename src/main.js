e3cbfb4a8ca9037722221939ce1053131f4eb492
codetribute <dotsolomon95@gmail.com>
2019-11-25 12:31:38 +0100

    updated router and login.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from "vue";
+import login from "./routes/Login.js"
 import App from './App.vue';
- 
+import VueRouter from "vue-router";
 
-new Vue({
-    el: '#app',
-    render: h => h(App),
+const routes = [
+    login.route
+];
+
+const router = new VueRouter({
+    routes // short for `routes: routes`
 });
+
+Vue.use(VueRouter);
+
+const app = new Vue({
+    router
+}).$mount('#app');
